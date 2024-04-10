48b133818426fa804c1c50f37c316921825565ca
Tiwar02 <salazarstiwar@gmail.com>
2024-04-09 21:56:46 -0500

    Products Crud Ready

--- Shopping/Shopping/Data/SeedDb.cs
+++ Shopping/Shopping/Data/SeedDb.cs
@@ -1,4 +1,5 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
 using Shopping.Data.Entities;
 using Shopping.Enums;
 using Shopping.Helpers;
@@ -9,11 +10,13 @@ namespace Shopping.Data
     {
         private readonly DataContext _context;
         private readonly IUserHelper _userHelper;
+        private readonly IBlobHelper _blobHelper;
 
-        public SeedDb(DataContext context, IUserHelper userHelper)
+        public SeedDb(DataContext context, IUserHelper userHelper, IBlobHelper blobHelper)
         {
             _context = context;
             _userHelper = userHelper;
+            _blobHelper = blobHelper;
         }
 
         public async Task SeedAsync()
@@ -22,9 +25,54 @@ namespace Shopping.Data
             await CheckCategoriesAsync();
             await CheckCountries();
             await CheckRolesAsync();
-            await CheckUserAsync("1010", "Pablito", "Perez", "Pape@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", UserType.Admin);
-            await CheckUserAsync("3030", "Elkin", "Tico", "elkintico@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", UserType.User);
+            await CheckUserAsync("001", "Admin", "Admin", "adminshop@yopmail.com", "322 311 4620", "Ed Principal", "admin.png", UserType.Admin);
+            await CheckUserAsync("1010", "Lionel", "Messi", "messi@yopmail.com", "304 221 3640", "Miami", "MESSI.jpg", UserType.User);
+            await CheckUserAsync("2020", "Cristiano", "Ronaldo", "cr7@yopmail.com", "344 255 4532", "Madrid Vallecas", "cr7.jpg", UserType.User);
+            await CheckUserAsync("3030", "Miguel", "Dos Santos", "miguelito@yopmail.com", "322 311 4620", "Calle 10", "user3.png", UserType.User);
+            await CheckProductsAsync();
         }
+
+        private async Task CheckProductsAsync()
+        {
+            if (!_context.Products.Any())
+            {
+                await AddProductAsync("Camisa Hombre", 270000M, 12F, new List<string>() { "Ropa" }, new List<string>() { "camisa hombre.png" });
+                await AddProductAsync("Camisa Atletico Nacional 2006", 450000M, 12F, new List<string>() { "Ropa", "Deportes" }, new List<string>() { "Camisanac-antique.png" });
+                await AddProductAsync("Jordan 1 DIOR", 13000000M, 12F, new List<string>() { "Ropa", "Calzado" }, new List<string>() { "j1 dior.png", "j1 dior 2.jpg" });
+                await AddProductAsync("Jordan 1 University Blue", 870000M, 12F, new List<string>() { "Ropa", "Calzado" }, new List<string>() { "j1 ublue.png" });
+                await AddProductAsync("Mouse Gamer Kalley", 100000M, 12F, new List<string>() { "Tecnologia", "Gamer" }, new List<string>() { "mouse.png" });
+                await AddProductAsync("Samsung S20", 2300000M, 12F, new List<string>() { "Tecnologia", "Celulares" }, new List<string>() { "s201.png", "s202.png", "s203.png" });
+                await _context.SaveChangesAsync();
+            }
+
+        }
+        private async Task AddProductAsync(string name, decimal price, float stock, List<string> categories, List<string> images)
+        {
+            Product prodcut = new()
+            {
+                Description = name,
+                Name = name,
+                Price = price,
+                Stock = stock,
+                ProductCategories = new List<ProductCategory>(),
+                ProductImages = new List<ProductImage>()
+            };
+
+            foreach (string? category in categories)
+            {
+                prodcut.ProductCategories.Add(new ProductCategory { Category = await _context.Categories.FirstOrDefaultAsync(c => c.Name == category) });
+            }
+
+
+            foreach (string? image in images)
+            {
+                Guid imageId = await _blobHelper.UploadBlobAsync($"{Environment.CurrentDirectory}\\wwwroot\\images\\products\\{image}", "products");
+                prodcut.ProductImages.Add(new ProductImage { ImageId = imageId });
+            }
+
+            _context.Products.Add(prodcut);
+        }
+
 
         private async Task<User> CheckUserAsync(
             string document,
@@ -33,11 +81,13 @@ namespace Shopping.Data
             string email,
             string phone,
             string address,
+            string image,
             UserType userType)
                 {
                     User user = await _userHelper.GetUserAsync(email);
                     if (user == null)
                     {
+                        Guid imageId = await _blobHelper.UploadBlobAsync($"{Environment.CurrentDirectory}\\wwwroot\\images\\users\\{image}", "users");
                         user = new User
                         {
                             FirstName = firstName,
@@ -49,6 +99,7 @@ namespace Shopping.Data
                             Document = document,
                             City = _context.Cities.FirstOrDefault(),
                             UserType = userType,
+                            ImageId= imageId
                         };
 
                         await _userHelper.AddUserAsync(user, "123456");
@@ -146,9 +197,11 @@ namespace Shopping.Data
                 _context.Categories.Add(new Category { Name = "Ropa" });
                 _context.Categories.Add(new Category { Name = "Calzado" });
                 _context.Categories.Add(new Category { Name = "Belleza" });
+                _context.Categories.Add(new Category { Name = "Gamer" });
                 _context.Categories.Add(new Category { Name = "Nutricíon" });
                 _context.Categories.Add(new Category { Name = "Deportes" });
                 _context.Categories.Add(new Category { Name = "Mascotas" });
+                _context.Categories.Add(new Category { Name = "Celulares" });
                 await _context.SaveChangesAsync();
             }    
         }
