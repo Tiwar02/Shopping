4c2f67cad16cb1dbfe72cc1254f4928649feab86
Tiwar02 <salazarstiwar@gmail.com>
2024-04-10 16:20:36 -0500

    Zoom to images/Improve Home

--- Shopping/Shopping/Data/DataContext.cs
+++ Shopping/Shopping/Data/DataContext.cs
@@ -19,7 +19,8 @@ namespace Shopping.Data
         public DbSet<ProductCategory> ProductCategories { get; set; }
         public DbSet<ProductImage> ProductImages { get; set; }
         public DbSet<State> States { get; set; }
-        
+        public DbSet<TemporalSale> TemporalSales { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
